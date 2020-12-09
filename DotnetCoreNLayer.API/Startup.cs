d594e7e996e901e00b3b57155f14b7cef2c67ba5
Adil Cem Albayrak <acalbayrak@tp.gov.tr>
2020-12-09 14:10:57 +0300

    Add: Connection string added and startup configuration made. Initial migration taken.

--- DotnetCoreNLayer.API/Startup.cs
+++ DotnetCoreNLayer.API/Startup.cs
@@ -1,7 +1,11 @@
+using DotnetCoreNLayer.Core.UnitOfWork;
+using DotnetCoreNLayer.Data;
+using DotnetCoreNLayer.Data.UnitOfWorks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +30,14 @@ namespace DotnetCoreNLayer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:DevelopmentConnection"].ToString(),
+                    o => {
+                        o.MigrationsAssembly("DotnetCoreNLayer.Data");
+                    });
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
